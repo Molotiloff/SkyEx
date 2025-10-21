c6a5cc5ce857836f14a00df4de84c7e9d45497d1
molotiloff <danil.motor9876@gmail.com>
2025-10-22 00:03:06 +0500

    new features

--- utils/req_index.py
+++ utils/req_index.py
@@ -3,16 +3,19 @@ from __future__ import annotations
 from collections import OrderedDict
 from dataclasses import dataclass
 
+
 @dataclass
 class ReqLink:
     bot_msg_id: int
     req_id: str
+
 
 class ReqIndex:
     """
     Память O(N) с LRU. Ключ: (chat_id, user_cmd_msg_id) -> ReqLink(bot_msg_id, req_id)
     Нужна, чтобы уметь редактировать заявку, если ответили на исходную команду.
     """
+
     def __init__(self, maxsize: int = 5000) -> None:
         self._m: "OrderedDict[tuple[int,int], ReqLink]" = OrderedDict()
         self._max = maxsize
@@ -31,5 +34,6 @@ class ReqIndex:
             # освежим LRU
             self._m.move_to_end(key)
         return v
+
 
 req_index = ReqIndex()
